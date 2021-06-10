eaaaac0e953876bc4ddb441f643aaa196b8da00e
Alexey <alexerisov@gmail.com>
2021-06-10 13:42:24 +0700

    upd: Add edit button to rows

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import './App.css';
 import DataTable from './DataTable';
 
 const darkTheme = createMuiTheme({
   palette: {
     type: 'dark',
+      primary: {
+          main: '#90caf9',
+      },
+      secondary: {
+          main: '#90caf9',
+      },
   },
 });
 
 const App = () => {
     return (
       <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
         <DataTable />
       </ThemeProvider>
     )
--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -1,23 +1,38 @@
 import * as React from 'react';
+import { Button } from '@material-ui/core'
 import { DataGrid } from '@material-ui/data-grid';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'firstName', headerName: 'First name', width: 130 },
     { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
+    { field: 'age', headerName: 'Age', type: 'number', width: 90 },
     {
         field: 'fullName',
         headerName: 'Full name',
+        flex: 1,
         description: 'This column has a value getter and is not sortable.',
         sortable: false,
         width: 160,
-    }
+        valueGetter: (params) =>
+            `${params.getValue(params.id, 'firstName') || ''} ${
+                params.getValue(params.id, 'lastName') || ''
+            }`,
+    },
+    {
+        field: ' ',
+        sortable: false,
+        width: 100,
+        renderCell: () => (
+                <Button
+                    color="primary"
+                    size="small"
+                    style={{ margin: 'auto' }}
+                >
+                    Edit
+                </Button>
+        ),
+    },
 ];
 
 const rows = [
@@ -35,7 +50,7 @@ const rows = [
 export default function DataTable() {
     return (
         <div style={{ height: 400, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+            <DataGrid rows={rows} columns={columns} pageSize={5} />
         </div>
     );
 }
